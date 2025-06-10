c2291b86829e20a20aa4d892fc76ea2590822aa4
Gustavo <gu1804@hotmail.com>
2025-06-09 23:46:02 -0300

    Initial commit: Laravel + Vue 3 + Inertia.js setup with Docker

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,21 @@
+<template>
+    <AppLayout>
+        <template #header>
+            Welcome
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 text-gray-900">
+                        Welcome to your new Inertia.js + Vue 3 application!
+                    </div>
+                </div>
+            </div>
+        </div>
+    </AppLayout>
+</template>
+
+<script setup>
+import AppLayout from '@/Layouts/AppLayout.vue';
+</script> 
